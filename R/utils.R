5052b65c3690892ee42d0e96d51b79b24bc30128
Iñaki Úcar <iucar@fedoraproject.org>
2023-02-11 19:55:40 +0100

    refactor integration code

---
+++ R/utils.R
@@ -0,0 +1,51 @@
+pkg_deps <- function(pkgs, db, ..., all=TRUE) {
+  inst <- row.names(utils::installed.packages(.Library.site, ...))
+  pkgs <- tools::package_dependencies(pkgs, db, recursive=TRUE)
+  pkgs <- lapply(pkgs, function(x) setdiff(x, inst))
+  pkgs <- unique(c(if (all) names(pkgs), unlist(pkgs, use.names=FALSE)))
+  pkgs
+}
+
+# adapted from install.packages
+check_versions <- function(pkgs, db) {
+  dbb <- available_sys()
+  row.names(dbb) <- tolower(row.names(dbb))
+
+  bins <- pkgs[tolower(pkgs) %in% row.names(dbb)]
+  srcs <- pkgs[! pkgs %in% bins]
+
+  binvers <- dbb[tolower(bins), "Version"]
+  srcvers <- sapply(bins, function(bin) # may not be in db
+    if (bin %in% row.names(db)) db[bin, "Version"] else "0")
+
+  later <- as.numeric_version(binvers) < srcvers
+
+  list(bins=bins, srcs=srcs, binvers=binvers, srcvers=srcvers, later=later)
+}
+
+# adapted from install.packages
+ask_user <- function(later, bins, binvers, srcvers) {
+  if (!any(later)) return(later)
+
+  msg <- ngettext(
+    sum(later),
+    "There is a binary version available but the source version is later",
+    "There are binary versions available but the source versions are later")
+  cat("\n", paste(strwrap(msg, indent = 2, exdent = 2), collapse = "\n"),
+      ":\n", sep = "")
+  print(data.frame(`binary` = binvers, `source` = srcvers,
+                   row.names = bins, check.names = FALSE)[later, ])
+  cat("\n")
+  action <- getOption("install.packages.compile.from.source", "interactive")
+  if (action == "interactive" && interactive()) {
+    msg <- gettext("Do you prefer later versions from sources?")
+    res <- utils::askYesNo(msg)
+    if (is.na(res)) stop("Cancelled by user")
+    if (!isTRUE(res)) later <- FALSE
+  } else if (action == "never") {
+    cat("  Binaries will be installed\n")
+    later <- FALSE
+  }
+
+  later
+}
