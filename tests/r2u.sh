651dc1a12084f1814ea5a2258d505456f7c65250
Iñaki Úcar <iucar@fedoraproject.org>
2023-03-16 12:28:54 +0100

    add r2u tests

--- tests/r2u.sh
+++ tests/r2u.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
 set -e
-echo "r2u tests"
+echo "options(bspm.version.check=FALSE)" >> /etc/R/Rprofile.site
+
+echo "TEST: install GitHub package"
+installGithub.r MangoTheCat/visualTest
+
+echo "TEST: install Bioc package"
+install.r BiocParallel
+
+echo "TEST: install binary deps + binary from r-universe"
+install2.r -r "https://eddelbuettel.r-universe.dev/bin/linux/jammy/4.2" tiledbsoma
